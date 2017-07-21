623d147e3ea0641aa15a7b3a6bb1d5b803884370
Jason Casey <jason.casey@primelineexpress.co.uk>
2017-07-21 10:26:33 +0100

    Initial Commit

--- PLE Process Monitor/Form1.Designer.vb
+++ PLE Process Monitor/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class PLEProcessMonitor
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,202 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(PLEProcessMonitor))
+        Me.BtnAzyra = New System.Windows.Forms.Button()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.lblAzyra = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblLastAzyra = New System.Windows.Forms.Label()
+        Me.lblNextAzyra = New System.Windows.Forms.Label()
+        Me.BtnTimedMonitor = New System.Windows.Forms.Button()
+        Me.lblNextTimedData = New System.Windows.Forms.Label()
+        Me.lblLastTimedData = New System.Windows.Forms.Label()
+        Me.lblTimedMonitor = New System.Windows.Forms.Label()
+        Me.tblReportingTimes = New System.Data.DataSet()
+        Me.tblReport = New System.Data.DataTable()
+        Me.colProcess = New System.Data.DataColumn()
+        Me.colTime = New System.Data.DataColumn()
+        CType(Me.tblReportingTimes, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.tblReport, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BtnAzyra
+        '
+        Me.BtnAzyra.BackColor = System.Drawing.Color.DarkRed
+        Me.BtnAzyra.FlatAppearance.BorderSize = 0
+        Me.BtnAzyra.ForeColor = System.Drawing.Color.Transparent
+        Me.BtnAzyra.Location = New System.Drawing.Point(33, 57)
+        Me.BtnAzyra.Name = "BtnAzyra"
+        Me.BtnAzyra.Size = New System.Drawing.Size(40, 40)
+        Me.BtnAzyra.TabIndex = 0
+        Me.BtnAzyra.UseVisualStyleBackColor = False
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStatus.Location = New System.Drawing.Point(29, 27)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(62, 20)
+        Me.lblStatus.TabIndex = 1
+        Me.lblStatus.Text = "Status"
+        '
+        'lblAzyra
+        '
+        Me.lblAzyra.AutoSize = True
+        Me.lblAzyra.Location = New System.Drawing.Point(79, 71)
+        Me.lblAzyra.Name = "lblAzyra"
+        Me.lblAzyra.Size = New System.Drawing.Size(38, 13)
+        Me.lblAzyra.TabIndex = 2
+        Me.lblAzyra.Text = "Azyra"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.Window
+        Me.Label1.Location = New System.Drawing.Point(255, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(119, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Last Checked"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.Window
+        Me.Label2.Location = New System.Drawing.Point(506, 27)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Next Check"
+        '
+        'lblLastAzyra
+        '
+        Me.lblLastAzyra.AutoSize = True
+        Me.lblLastAzyra.Location = New System.Drawing.Point(256, 71)
+        Me.lblLastAzyra.Name = "lblLastAzyra"
+        Me.lblLastAzyra.Size = New System.Drawing.Size(38, 13)
+        Me.lblLastAzyra.TabIndex = 5
+        Me.lblLastAzyra.Text = "Azyra"
+        '
+        'lblNextAzyra
+        '
+        Me.lblNextAzyra.AutoSize = True
+        Me.lblNextAzyra.Location = New System.Drawing.Point(507, 71)
+        Me.lblNextAzyra.Name = "lblNextAzyra"
+        Me.lblNextAzyra.Size = New System.Drawing.Size(38, 13)
+        Me.lblNextAzyra.TabIndex = 5
+        Me.lblNextAzyra.Text = "Azyra"
+        '
+        'BtnTimedMonitor
+        '
+        Me.BtnTimedMonitor.BackColor = System.Drawing.Color.DarkRed
+        Me.BtnTimedMonitor.FlatAppearance.BorderSize = 0
+        Me.BtnTimedMonitor.ForeColor = System.Drawing.Color.Transparent
+        Me.BtnTimedMonitor.Location = New System.Drawing.Point(33, 116)
+        Me.BtnTimedMonitor.Name = "BtnTimedMonitor"
+        Me.BtnTimedMonitor.Size = New System.Drawing.Size(40, 40)
+        Me.BtnTimedMonitor.TabIndex = 6
+        Me.BtnTimedMonitor.UseVisualStyleBackColor = False
+        '
+        'lblNextTimedData
+        '
+        Me.lblNextTimedData.AutoSize = True
+        Me.lblNextTimedData.Location = New System.Drawing.Point(507, 130)
+        Me.lblNextTimedData.Name = "lblNextTimedData"
+        Me.lblNextTimedData.Size = New System.Drawing.Size(109, 13)
+        Me.lblNextTimedData.TabIndex = 9
+        Me.lblNextTimedData.Text = "TimedMonitorNext"
+        '
+        'lblLastTimedData
+        '
+        Me.lblLastTimedData.AutoSize = True
+        Me.lblLastTimedData.Location = New System.Drawing.Point(256, 130)
+        Me.lblLastTimedData.Name = "lblLastTimedData"
+        Me.lblLastTimedData.Size = New System.Drawing.Size(107, 13)
+        Me.lblLastTimedData.TabIndex = 8
+        Me.lblLastTimedData.Text = "TimedMonitorLast"
+        '
+        'lblTimedMonitor
+        '
+        Me.lblTimedMonitor.AutoSize = True
+        Me.lblTimedMonitor.Location = New System.Drawing.Point(79, 130)
+        Me.lblTimedMonitor.Name = "lblTimedMonitor"
+        Me.lblTimedMonitor.Size = New System.Drawing.Size(118, 13)
+        Me.lblTimedMonitor.TabIndex = 7
+        Me.lblTimedMonitor.Text = "Timed Monitor Data"
+        '
+        'tblReportingTimes
+        '
+        Me.tblReportingTimes.DataSetName = "ReportingTimes"
+        Me.tblReportingTimes.Tables.AddRange(New System.Data.DataTable() {Me.tblReport})
+        '
+        'tblReport
+        '
+        Me.tblReport.Columns.AddRange(New System.Data.DataColumn() {Me.colProcess, Me.colTime})
+        Me.tblReport.Constraints.AddRange(New System.Data.Constraint() {New System.Data.UniqueConstraint("Constraint1", New String() {"ProcessName"}, True)})
+        Me.tblReport.PrimaryKey = New System.Data.DataColumn() {Me.colProcess}
+        Me.tblReport.TableName = "ReportingTimes"
+        '
+        'colProcess
+        '
+        Me.colProcess.AllowDBNull = False
+        Me.colProcess.ColumnName = "ProcessName"
+        '
+        'colTime
+        '
+        Me.colTime.AllowDBNull = False
+        Me.colTime.ColumnName = "NextRun"
+        Me.colTime.DataType = GetType(Date)
+        '
+        'PLEProcessMonitor
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(736, 493)
+        Me.Controls.Add(Me.lblNextTimedData)
+        Me.Controls.Add(Me.lblLastTimedData)
+        Me.Controls.Add(Me.lblTimedMonitor)
+        Me.Controls.Add(Me.BtnTimedMonitor)
+        Me.Controls.Add(Me.lblNextAzyra)
+        Me.Controls.Add(Me.lblLastAzyra)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblAzyra)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.BtnAzyra)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.White
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "PLEProcessMonitor"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "PLE Process Monitor"
+        CType(Me.tblReportingTimes, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.tblReport, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents BtnAzyra As Button
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents lblAzyra As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblLastAzyra As Label
+    Friend WithEvents lblNextAzyra As Label
+    Friend WithEvents BtnTimedMonitor As Button
+    Friend WithEvents lblNextTimedData As Label
+    Friend WithEvents lblLastTimedData As Label
+    Friend WithEvents lblTimedMonitor As Label
+    Friend WithEvents tblReportingTimes As DataSet
+    Friend WithEvents tblReport As DataTable
+    Friend WithEvents colProcess As DataColumn
+    Friend WithEvents colTime As DataColumn
 End Class
--- PLE Process Monitor/Form1.vb
+++ PLE Process Monitor/Form1.vb
@@ -1,3 +1,188 @@
-﻿Public Class Form1
+﻿Imports System.Timers
+Public Class PLEProcessMonitor
+    Public Declare Sub mouse_event Lib "user32" (ByVal dwFlags As Integer, ByVal dx As Integer, ByVal dy As Integer, ByVal cButtons As Integer, ByVal dwExtraInfo As Integer)
+    Public Declare Function SetCursorPos Lib "user32" (ByVal X As Integer, ByVal Y As Integer) As Integer
+    Public Const MOUSEEVENTF_LEFTDOWN As Integer = &H2
+    Public Const MOUSEEVENTF_LEFTUP As Integer = &H4
+    Public Const MOUSEEVENTF_MIDDLEDOWN As Integer = &H20
+    Public Const MOUSEEVENTF_MIDDLEUP As Integer = &H40
+    Public Const MOUSEEVENTF_RIGHTDOWN As Integer = &H8
+    Public Const MOUSEEVENTF_RIGHTUP As Integer = &H10
+    Public AzyraRow As DataRow
+    Public TimedMonitorRow As DataRow
+    Private Sub PLEProcessMonitor_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Show()
+        Application.DoEvents()
+        Call FirstRun()
+    End Sub
+
+    Private Sub CheckTimedEvent(source As Object, e As ElapsedEventArgs)
+        Call DoStuffThread()
+    End Sub
+
+    Private Sub TimedMonitorTimedEvent(source As Object, e As ElapsedEventArgs)
+        Call ExportTimedMonitorData()
+    End Sub
+    Private Sub BtnAzyra_Click(sender As Object, e As EventArgs) Handles BtnAzyra.Click
+        Call AzyraChecker()
+    End Sub
+
+    Private Function CheckAzyra() As Boolean
+        Try
+            AppActivate("Azyra - Primeline Live")
+            'SendKeys.SendWait("~")
+            BtnAzyra.BackColor = Color.DarkGreen
+            lblAzyra.Text = "Azyra running"
+            CheckAzyra = True
+        Catch ex As Exception
+            BtnAzyra.BackColor = Color.DarkRed
+            lblAzyra.Text = "Azyra not running"
+            CheckAzyra = False
+        End Try
+        lblLastAzyra.Text = DateTime.Now
+        AzyraRow("NextRun") = DateTime.Now.AddSeconds(5)
+        lblNextAzyra.Text = AzyraRow("NextRun").ToString
+    End Function
+    Public Sub AzyraChecker()
+        If Not CheckAzyra() Then
+            Call LaunchAzyra()
+        End If
+        Show()
+    End Sub
+    Private Sub LaunchAzyra()
+        Dim proc As New System.Diagnostics.Process()
+        proc = Process.Start("\\APOLLO1\Azyra\Azyra.EXE", "")
+        Threading.Thread.Sleep(3000)
+        AppActivate("Azyra")
+        SendKeys.SendWait("{TAB}")
+        SendKeys.SendWait("+jason +casey")
+        'SendKeys.SendWait("a")
+        'SendKeys.SendWait("s")
+        'SendKeys.SendWait("o")
+        'SendKeys.SendWait("n")
+        'SendKeys.SendWait(" ")
+        'SendKeys.SendWait("^c")
+        'SendKeys.SendWait("a")
+        'SendKeys.SendWait("s")
+        'SendKeys.SendWait("e")
+        'SendKeys.SendWait("y")
+        SendKeys.SendWait("{TAB}")
+        SendKeys.SendWait("teacher")
+        'SendKeys.SendWait("e")
+        'SendKeys.SendWait("a")
+        'SendKeys.SendWait("c")
+        'SendKeys.SendWait("h")
+        'SendKeys.SendWait("e")
+        'SendKeys.SendWait("r")
+        SendKeys.SendWait("~")
+    End Sub
+
+    Private Sub BtnTimedMonitor_Click(sender As Object, e As EventArgs) Handles BtnTimedMonitor.Click
+        Call ExportTimedMonitorData()
+    End Sub
+
+    Public Sub ExportTimedMonitorData()
+        Dim strExportFrom As String
+        strExportFrom = Format(AddWorkingDays(Today(), -2), "dd/MM/yy")
+        AppActivate("Azyra")
+        SendKeys.SendWait("~")
+        Threading.Thread.Sleep(3000)
+        'Right click 75, 140 FREIGHT
+        SetCursorPos(75, 140) 'x and y position
+        mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
+        'Left click 170, 150 Freight Menu
+        SetCursorPos(170, 150) 'x and y position
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        'Left click 200, 500 Reporting
+        SetCursorPos(200, 500) 'x and y position
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        'Double click 500, 170 Job Reporting Spreadsheet
+        SetCursorPos(500, 170) 'x and y position
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        'Send TAB
+        Threading.Thread.Sleep(2000)
+        SendKeys.SendWait("{TAB}")
+        'Send Ctrl+V, 2 working days ago
+        'ActiveWorkbook.Sheets("Controls").Range("C1").Copy
+        SendKeys.SendWait(strExportFrom)
+        Threading.Thread.Sleep(2000)
+        'Right click 575, 310 calendar
+        SetCursorPos(575, 310) 'x and y position
+        mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
+        'Left click 625, 320 TODAY
+        SetCursorPos(625, 320) 'x and y position
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        'Left click 800, 635 EXPORT
+        SetCursorPos(800, 635) 'x and y position
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        'Left click 530, 355 xml
+        SetCursorPos(530, 355) 'x and y position
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        'Send TAB
+        SendKeys.SendWait("{TAB}")
+        'Send testjobdata.xml
+        SendKeys.SendWait("testjobdata.xml")
+        'Left click 570, 540 EXPORT
+        SetCursorPos(570, 540) 'x and y position
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        'Wait 45 seconds
+        Threading.Thread.Sleep(45000)
+        'Send enter
+        SendKeys.SendWait("{enter}")
+        'Left click 155, 680 BACK
+        SetCursorPos(155, 680) 'x and y position
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        'Left click 155, 680 CLOSE
+        SetCursorPos(155, 680) 'x and y position
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        Activate()
+        lblLastTimedData.Text = DateTime.Now
+        TimedMonitorRow("NextRun") = DateTime.Now.AddMinutes(60)
+        lblNextTimedData.Text = TimedMonitorRow("NextRun").ToString
+        Show()
+    End Sub
+    Public Function AddWorkingDays(ByVal DateIn As DateTime,
+        ByVal ShiftDate As Integer) As DateTime
+        ' Adds the [ShiftDate] number of working days to DateIn
+        Dim datDate As DateTime = DateIn.AddDays(ShiftDate)
+        ' Loop around until we get the need non-weekend day
+        While Weekday(datDate) = 1 Or Weekday(datDate) = 7
+            datDate = datDate.AddDays(IIf(ShiftDate < 0, -1, 1))
+        End While
+        Return datDate
+    End Function
+    Private Sub DoStuffThread()
+        If AzyraRow("NextRun") < Now Then
+            Call AzyraChecker()
+        End If
+        If TimedMonitorRow("NextRun") < Now Then
+            Call ExportTimedMonitorData()
+        End If
+    End Sub
+
+    Private Sub FirstRun()
+        AzyraRow = tblReport.Rows.Add(New Object() {"AzyraChecker", "05:48"})
+        TimedMonitorRow = tblReport.Rows.Add(New Object() {"ExportTimedMonitorData", "15:32"})
+        lblNextAzyra.Text = AzyraRow("NextRun").ToString
+        lblNextTimedData.Text = TimedMonitorRow("NextRun").ToString
+        Dim TimerCheck As System.Timers.Timer = New Timer((2 * 60000))
+        AddHandler TimerCheck.Elapsed, AddressOf CheckTimedEvent
+        TimerCheck.Enabled = True
+        Call AzyraChecker()
+        Call DoStuffThread()
+    End Sub
 
 End Class
